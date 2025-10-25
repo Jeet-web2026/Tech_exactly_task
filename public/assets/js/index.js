2051c8bff8d551e608ba86e510230ca1acd8b3f2
Jeet <jeetnath2110@gmail.com>
2025-10-25 14:44:15 +0530

    Initial changes on maincontent

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -53,4 +53,36 @@ $(document).ready(function () {
         autoplay: true,
         autoplaySpeed: 2000,
     });
+
+    $(".editors-pick-showcase").slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
 });
