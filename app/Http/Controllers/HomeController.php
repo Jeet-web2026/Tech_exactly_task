929719e8d1b9d6c5e939ae92d915edc6ebb3e4d3
Jeet <jeetnath2110@gmail.com>
2025-10-26 13:46:27 +0530

    Home page dynamic running

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Post;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $Posts = Cache::remember('all_latest_posts', now()->addMinutes(5), function () {
+            return Post::with('user:id,fname,lname')
+                ->latest()
+                ->take(5)
+                ->get(['user_id', 'id', 'title', 'content', 'category', 'image', 'created_at']);
+        });
+
+        $singlePost = $Posts->first();
+        $secondaryPost = $Posts->take(4);
+        $techPosts = Cache::remember('all_latest_tech_news', now()->addMinutes(5), function () {
+            return Post::with('user:id,fname,lname')
+                ->where('category', 'Tech')
+                ->latest()
+                ->take(3)
+                ->get(['user_id', 'id', 'title', 'content', 'category', 'image', 'created_at']);
+        });
+
+        return view('index', compact(
+            'Posts',
+            'singlePost',
+            'secondaryPost',
+            'techPosts'
+        ));
     }
 }
